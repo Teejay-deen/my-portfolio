c693dc39becd4e2304dfbd165058cb73c4b130cf
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-09-19 22:56:25 +0100

    added nav bar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
-import Link from 'next/link'
-import { Button } from './ui/button'
+import React from "react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header className='py-8 xl:py-12 text-white'>
-        <div className='container mx-auto'>
-            <Link href="/">
-            <h1 className='text-4xl font-semibold '>Teejay <span className='text-emerald-500'>.</span></h1>
-            </Link>
-        </div>
-    </header>
-  )
-}
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold ">
+            Teejay <span className="text-emerald-500">.</span>
+          </h1>
+        </Link>
 
-export default Header
+        {/* Desktop nav */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+
+        {/* Mobile nav */}
+
+        <div className="xl:hidden ">Mobile Nav</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
