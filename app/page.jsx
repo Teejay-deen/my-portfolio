fe486915857423f5e39451b69a2229944314a16f
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-09-22 22:08:29 +0100

    conflict resloved

--- app/page.jsx
+++ app/page.jsx
@@ -7,27 +7,23 @@ import { MdOutlineFileDownload } from "react-icons/md";
 const Home = () => {
   return (
     <section className="h-full ">
-      <div className=" container mx-auto h-full">
+      <div className="container mx-auto h-full">
         <div className="flex justify-between flex-col xl:flex-row items-center xl:pt-8 xl:pb-24">
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              {" "}
-              Hello{"I'm"}<br />{" "}
-              <span className="text-emerald-500">Jimoh Tajudeen </span>{" "}
+              Hello {"I'm"} <br />
+              <span className="text-emerald-500">Jimoh Tajudeen </span>
             </h1>
-
             <p className="max-w-[500px] mb-9 text-white/80">
-              I excel at crafting elegant digit experienes and i am proficient
-              in various programming language and technologies{" "}
+              I excel at crafting elegant digital experiences and I am proficient
+              in various programming languages and technologies.
             </p>
-
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button className="uppercase flex items-center gap-2" size="lg">
                 <span>Download CV</span>
                 <MdOutlineFileDownload className="text-xl" />
               </Button>
-
               <div className="mb-8 xl:mb-0">
                 <Socials
                   containerStyles="flex gap-6"
