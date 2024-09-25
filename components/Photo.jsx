9f9f98910a1321f1a5b0936abf935a7056358467
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-09-25 21:19:54 +0100

    potential image fix

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -21,9 +21,9 @@ const Photo = () => {
           }}
           className="w-[250px] h-[250px] xl:w-[450px] xl:h-[450px] rounded-full overflow-hidden flex items-center justify-center mix-blend-lighten"
         >
-          <Image
+          <img
             className="object-cover"
-            src="/assets/teejay.jpg"
+            src="./assets/teejay.jpg"
             priority
             quality={100}
             alt="Profile Image"
