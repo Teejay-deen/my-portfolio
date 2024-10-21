a0888bd9c6874bb593f5e4fbb92be25b1e0049b7
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-10-21 15:37:07 +0100

    making the a About me to br cursor pointer

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -323,7 +323,7 @@ const Resume = () => {
                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
                   {about.description}
                 </p>
-                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                <ul className="grid cursor-pointer grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
                   {about.info.map((item, index) => {
                     return (
                       <li
