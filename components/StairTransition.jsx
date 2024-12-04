d7a6e92f8f933a7242ba6b3eec46b9557903fc86
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-12-04 13:13:14 +0000

    refactor

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -14,7 +14,7 @@ const StairTransition = () => {
           </div>
 
           <motion.div
-            className="h-screen  w-screen fixed bg-primary top-0 pointer-events-none"
+            className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
             initial={{ opacity: 1 }}
             animate={{
               opacity: 0,
