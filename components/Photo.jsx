bdf28d8d3c4335683508f569b383fa776a68088c
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-12-13 22:29:48 +0000

    revert

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -42,7 +42,6 @@ const Photo = () => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <motion.circle
-          
             cx="225" 
             cy="225" 
             r="215" 
