c0cad211daf229a76ba3583f29a94943613e6e1a
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-12-25 13:22:22 +0000

    fix

--- components/Social.jsx
+++ components/Social.jsx
@@ -17,7 +17,6 @@ const socials = [
 const Socials = ({containerStyles, iconStyles}) => {
   return <div className={containerStyles}>
     {socials.map((item, index)=>{
-      
         return (
             <Link className={iconStyles} href={item.path} key={index}>
                 {item.icons}
