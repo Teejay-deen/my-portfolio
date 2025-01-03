dc6bd8040722de29f35a671a2d55ae5c7e63def5
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-01-03 19:51:53 +0000

    fixes

--- app/page.jsx
+++ app/page.jsx
@@ -29,7 +29,6 @@ const Home = () => {
                 <Button className="uppercase flex items-center gap-2" size="lg">
                   <span>Download CV</span>
                   <MdOutlineFileDownload className="text-xl" />
-                  
                 </Button>
               </a>
               <div className="mb-8 xl:mb-0">
