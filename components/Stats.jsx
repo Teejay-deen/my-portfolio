0ea82bf6c40c1169255a13dd1fa1d4fa756dce4e
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-01-17 23:02:02 +0100

    refactor

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -28,10 +28,11 @@ const Stats = () => {
           {stats.map((item, index) => {
             return (
               <div
+              
                 className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
                 key={index}
               >
-              
+
                 <CountUp
                   end={item.num}
                   duration={5}
