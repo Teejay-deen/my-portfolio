09688208e3b74a6194e5994e1bb3f300a50d08de
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-02-22 15:22:42 +0100

    stats repaired

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -7,7 +7,7 @@ const stats = [
     text: "Years of experience",
   },
   {
-    num: 15,
+    num: 24,
     text: "Projects completed",
   },
   {
@@ -22,13 +22,13 @@ const stats = [
 
 const Stats = () => {
   return (
-    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+    <section className="pt- pb-12 xl:pt-0 xl:pb-0">
       <div className="container mx-auto">
         <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
           {stats.map((item, index) => {
             return (
               <div
-                className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
+                className="flex-1  flex gap-4 items-center justify-center xl:justify-start"
                 key={index}
               >
               
@@ -36,9 +36,8 @@ const Stats = () => {
                   end={item.num}
                   duration={5}
                   delay={2}
-                  className="text-4xl xl:text-6xl text-white font-extrabold"
+                  className="text-4xl font-roboto xl:text-6xl text-white font-extrabold"
                 />
-
                 <p className={`${item.text.length<15 ? "max-w-[100px] " : "max-w-[150px]"} leading-snug text-white/80`}>{item.text}</p>
               </div>
             );
