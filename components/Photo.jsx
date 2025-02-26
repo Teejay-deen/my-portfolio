2809bf5988d5244d37767a4378c38abdfc10a33a
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-02-26 19:26:40 +0100

    fixed

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -20,6 +20,7 @@ const Photo = () => {
           }}
           className="w-[250px] h-[250px]  xl:w-[450px] xl:h-[450px] rounded-full overflow-hidden flex items-center justify-center mix-blend-lighten"
         >
+          
           <Image
           
             className="object-cover"
