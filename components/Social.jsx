219f723fd75eb1d7756d7e9f5a91ef0507d84d9d
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-03-07 23:43:23 +0100

    fixes

--- components/Social.jsx
+++ components/Social.jsx
@@ -17,7 +17,6 @@ const Socials = ({containerStyles, iconStyles}) => {
   return <div className={containerStyles}>
     {socials.map((item, index)=>{
         return (
-        
             <Link className={iconStyles} href={item.path} key={index}>
                 {item.icons}
             </Link>
