31c755de184aceacec9876b71003a1a9e12de36c
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-05-10 08:47:07 +0100

    revert

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -6,8 +6,7 @@ import Stairs from "./Stairs";
 const StairTransition = () => {
   const pathname = usePathname();
   return (
-    <>
-    
+    <>   
       <AnimatePresence mode="wait">
         <div key={pathname}>
           <div className="h-screen fixed  w-screen top-0 right-0 left-0 pointer-events-none z-40 flex">
