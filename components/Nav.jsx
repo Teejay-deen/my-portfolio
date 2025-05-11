ac4901b0d40043756e40cc332a8157d981faa90e
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-05-11 09:54:15 +0100

    fixes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -30,6 +30,7 @@ const Nav = () => {
     <nav className="flex gap-8">
       {links.map((link, index) => (
         <Link
+        
           key={index}
           href={link.path}
           className={`${
