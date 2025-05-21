a367a4584d97625dc91bc27b7094f70d44e7eec5
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-05-21 10:15:40 +0100

    Change header name

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,12 +10,11 @@ const Header = () => {
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
           <h1 className="text-4xl font-semibold ">
-            Teejay<span className="text-emerald-500">.</span>
+            TAJ<span className="text-emerald-500">.</span>
           </h1>
         </Link>
         {/* Desktop nav */}
         
-    
         <div className="hidden xl:flex items-center gap-8">
           <Nav />
           <Link href="/contact">
