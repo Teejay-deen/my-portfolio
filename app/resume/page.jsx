c558695e2b93cb763e872b6cb54b029da6fe2703
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-06-23 23:00:50 +0100

    fixes

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -258,6 +258,7 @@ const Resume = () => {
                       return (
                         <li
                           key={index}
+                          
                           className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
                         >
                           <span className="text-emerald-500">
