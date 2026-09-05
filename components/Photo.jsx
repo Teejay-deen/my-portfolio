0d8c85aad11cb76cbc8791ee0c170d851dced01d
Teejay-deen <Jimoh.tajudeen1017@gmail.com>
2026-09-05 13:57:52 +0100

    fixed the bug

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -17,6 +17,7 @@ const Photo = () => {
         <motion.div
           initial={{ opacity: 0 }}
           animate={{
+          
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
