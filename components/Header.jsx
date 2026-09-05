d986169430a85baa1121b2117aed809279932187
Teejay-deen <Jimoh.tajudeen1017@gmail.com>
2026-09-05 14:06:04 +0100

    fixed

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,6 +6,7 @@ import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
+    
     <header className="py-8 xl:py-12 text-white">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
