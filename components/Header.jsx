cd690e57567d13da4f28f3252d56a3f84cc7bb0c
Teejay-deen <Jimoh.tajudeen1017@gmail.com>
2026-09-18 18:40:17 +0100

    fixed

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
           <h1 className="text-4xl font-semibold ">
             TAJ<span className="text-emerald-500">.</span>
           </h1>
-          
+           
         </Link>
         {/* Desktop nav */}
         
