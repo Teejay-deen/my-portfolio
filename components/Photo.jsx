72e8f3bdc5de4bc6d2684c2fc5a6dfab2f88d9da
Teejay-deen <Jimoh.tajudeen1017@gmail.com>
2026-09-21 07:59:07 +0100

    fixed

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -16,7 +16,7 @@ const Photo = () => {
       >
         <motion.div
           initial={{ opacity: 0 }}
-          animate={{
+          animate={{ 
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
